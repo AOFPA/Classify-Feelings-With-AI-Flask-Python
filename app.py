470cbfeade9346b6b1b4b0e8c08887b03c01eebb
AOFPA <chalongchaith@gmail.com>
2022-04-08 23:39:38 +0700

    config env

--- app.py
+++ app.py
@@ -1,33 +1,66 @@
-from flask import Flask, jsonify
+from flask import Flask,render_template,request,url_for
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+from pythainlp.corpus.common import thai_stopwords
+from pythainlp import word_tokenize
+from sklearn.model_selection import train_test_split
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix,classification_report
 
 app = Flask(__name__)
 
-data = [
-        {
-            "id": 1,
-            "library": "Pandas",
-            "language": "Python"
-        },
-        {
-            "id": 2,
-            "library": "requests",
-            "language": "Python"
-        },
-        {
-            "id": 3,
-            "library": "NumPy",
-            "language": "Python"
-        }
-    ]
+#train nlp
+thai_stopwords = list(thai_stopwords())
+df = pd.read_csv('review_shopping.csv', sep='\t', names=['text', 'sentiment'], header=None)
 
+def text_process(text):
+    final = "".join(u for u in text if u not in ("?", ".", ";", ":", "!", '"', "ๆ", "ฯ"))
+    final = word_tokenize(final)
+    final = " ".join(word for word in final)
+    final = " ".join(word for word in final.split() 
+                    if word.lower not in thai_stopwords)
+    return final
+
+df['text_tokens'] = df['text'].apply(text_process)
+
+X = df[['text_tokens']]
+y = df['sentiment']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+
+cvec = CountVectorizer(analyzer=lambda x:x.split(' '))
+cvec.fit_transform(X_train['text_tokens'])
+
+train_bow = cvec.transform(X_train['text_tokens'])
+
+lr = LogisticRegression()
+lr.fit(train_bow, y_train)
+test_bow = cvec.transform(X_test['text_tokens'])
+
+
+#flask
 @app.route('/')
-def hello():
-    return "Hello Flask-Heroku"
+def index():
+    return render_template('index.html',predic="")
+
+@app.route('/process', methods=['POST'])
+def nlp():
+    if request.method == 'POST':
+        message = request.form['message']
+        if message == '':
+            return render_template('index.html')
+        else:
+            my_text = [message]
+            my_tokens = text_process(my_text)
+            my_bow = cvec.transform(pd.Series([my_tokens]))
+            my_predictions = lr.predict(my_bow)
+
+            return render_template('index.html', predic = my_predictions[0],msg = my_text[0])
 
 
-@app.route('/api', methods=['GET'])
-def get_api():
-    return jsonify(data)
-
-if __name__ == "__main__":
-    app.run(debug=False)
+if __name__ == '__main__':
+    app.run(debug=True)
